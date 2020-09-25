f0e30e5a2e5bd05150ccdd4a43cc2150953e943f
黄江泰 <jtH20182019@163.com>
2020-09-25 16:27:34 +0800

    在idea中练习git使用

---
+++ src/main/java/com/atguigu/bean/Love.java
@@ -0,0 +1,14 @@
+package com.atguigu.bean;
+
+/**
+ * @ClassName: Love
+ * @Description:
+ * @Author: hjt
+ * @Date: Created in 2020/9/25
+ * @Version 1.0
+ */
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
